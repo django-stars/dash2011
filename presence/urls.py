61a343471e8e8e8a399123d4793fe13d7d429060
Anton Agafonov <anton.agafonov@gmail.com>
2011-07-30 11:20:04 +0300

    Project skeleton

--- presence/urls.py
+++ presence/urls.py
@@ -1,17 +1,23 @@
+from django.conf import settings
 from django.conf.urls.defaults import patterns, include, url
+from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+
+admin.autodiscover()
+
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'presence.views.home', name='home'),
-    # url(r'^presence/', include('presence.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
+
+
+if settings.LOCAL_DEVELOPMENT:
+    urlpatterns += staticfiles_urlpatterns()
+
+if settings.LOCAL_DEVELOPMENT:
+    urlpatterns += patterns("django.views",
+        url(r"%s(?P<path>.*)$" % settings.MEDIA_URL[1:], "static.serve", {
+            "document_root": settings.MEDIA_ROOT,
+        })
+    )
