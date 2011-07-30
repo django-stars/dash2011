00bf50fc79c220e10ee910edf652ac7ef4408b49
Roman Osipenko <roman.osipenko@djangostars.com>
2011-07-30 17:07:00 +0300

    Fix 'is_private' utils function behaviour.

--- presence/apps/shout/utils.py
+++ presence/apps/shout/utils.py
@@ -1,13 +1,14 @@
 def url2link(url):
-	from lxml import html
-	try:
-		title = html.parse(url).find(".//title").text
-		return title if title else url
-	except AttributeError:
-		return url
+    from lxml import html
+    try:
+        title = html.parse(url).find(".//title").text
+        return title if title else url
+    except AttributeError:
+        return url
 
 def is_private(message):
-	return message.startswith("!! "), message[3:]
+    result = message.startswith("!! ")
+    return result, message[3:] if result else message
 
 def user2link(message):
-	return message
+    return message
