7852b59e693301d7bfdbd9bc4a3a456b9909712e
Vasyl Dizhak <dijakroot@gmail.com>
2011-07-31 23:42:07 +0300

    Redirect to edit profile page after user activation

--- presence/apps/activation/views.py
+++ presence/apps/activation/views.py
@@ -6,15 +6,24 @@ from django.conf import settings
 
 from models import ActivationKey
 
+import logging
+
+logger = logging.getLogger("presence.%s" % __name__)
+
 
 def activate_user(request, key):
     try:
         _key = ActivationKey.objects.activate_user(key)
+        messages.info(request,
+            "Your account was activated, please change your profile!"
+            )
+        logger.info("User was activated with key %s" % key)
     except:
         messages.error(request, "Invalid key, please login with this form")
-        return HttpResponseRedirect(reverse("login"))
+        logger.debug("Invalid key %s" % key)
+        return HttpResponseRedirect(reverse("dashboard"))
     else:
         _user = authenticate(key=key)
         login(request, _user)
         _key.delete()
-        return HttpResponseRedirect(settings.LOGIN_REDIRECT_URL)
+        return HttpResponseRedirect(reverse("profile-edit"))
