4f9c9d08932d0a5123703d6d985e2ee0fc57099b
Vasyl Dizhak <dijakroot@gmail.com>
2011-07-31 19:26:12 +0300

    Added week and month plans

--- presence/apps/planning/templatetags/planning_tags.py
+++ presence/apps/planning/templatetags/planning_tags.py
@@ -8,10 +8,13 @@ register = template.Library()
 
 
 @register.inclusion_tag("planning/tags/plans-info.html", takes_context=True)
-def user_plans(context):
-    username = context['request'].user.username
+def user_plans(context, username):
     plans = DayPlan.objects.user_plans(username)
+    seven_days_plan = DayPlan.objects.user_plans(username, days=7)
+    thirty_days_plan = DayPlan.objects.user_plans(username, days=30)
     data = {
         'plans': plans,
+        'seven_days_plan': seven_days_plan,
+        'thirty_days_plan': thirty_days_plan
     }
     return data
