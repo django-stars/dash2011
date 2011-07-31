7c9334212ba25c6c8fae0903c4330cb0d82eb687
Roman Osipenko <roman.osipenko@djangostars.com>
2011-07-31 16:45:51 +0300

    Shout front-end improvements.

--- presence/static/js/presence.js
+++ presence/static/js/presence.js
@@ -1,9 +1,34 @@
 var Presence = (function(){
-	return {
-		init: function(){
-			// timeago activation;
-			$("time.relative").timeago();
-        } // init
+    return {
+        init: function(){
+            $("time.relative").timeago();
+            Presence.makeShout();
+        }, // init
+        makeShout: function(){
+            var top_shout = $("form.shout");
+            $("#shout-action").click(function(){
+                if (top_shout.is(":visible")){
+                    top_shout.slideUp();
+                }
+                else {
+                    top_shout.slideDown();
+                    top_shout.find("textarea").focus();
+                }
+                return false;
+            }); // click
+            top_shout.submit(function(){
+                var this_ = $(this);
+                $.post(
+                    this_.attr("action"),
+                    this_.serialize(),
+                    function(data){
+                        if (data){
+                            top_shout.slideUp();
+                        }
+                    }) // post
+                return false;
+            })
+        }
     }// main return
 })($);
 
