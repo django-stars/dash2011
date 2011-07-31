8b78434183e3248a450fa0f0d5ce80d65fead5f6
Vasyl Dizhak <dijakroot@gmail.com>
2011-08-01 02:36:49 +0300

    Added messages for voting

--- presence/apps/vote/views.py
+++ presence/apps/vote/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.core.urlresolvers import reverse
+from django.contrib import messages
 
 from models import UserVote
 from forms import UserVoteForm
@@ -13,6 +14,7 @@ def vote(request):
         if form.is_valid():
             vote = form.save(commit=False)
             vote = UserVote.objects.vote(request.user, vote.vote)
+            messages.info(request, "Your mood is %s" % vote.get_vote_display())
     else:
         form = UserVoteForm()
     return HttpResponseRedirect(reverse('dashboard'))
